c194a9f43bb6ad8e08ea682aa9fcd3ef9fde4f75
proyaev_vladislav <farmfilok@gmail.com>
2024-02-01 21:01:17 +0300

    add log / add goroutine

--- day_1/main.go
+++ day_1/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"day_1/api"
@@ -8,6 +9,14 @@ import (
 )
 
 func main() {
+	defaultHandler := http.DefaultServeMux
+	customHandler := http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			log.Println("Got request:", r.RequestURI)
+			defaultHandler.ServeHTTP(w, r)
+		},
+	)
+
 	info := &models.Info{
 		Name:    "API",
 		Version: "1.0.0",
@@ -35,8 +44,16 @@ func main() {
 	http.HandleFunc(
 		"/div", api.CalculatorHandler,
 	)
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		return
-	}
+
+	const serverPort = ":8080"
+	go func() {
+		err := http.ListenAndServe(serverPort, customHandler)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	log.Println("Server started: http://localhost" + serverPort)
+	log.Println("Press Ctrl+C to stop the server")
+	select {}
 }
